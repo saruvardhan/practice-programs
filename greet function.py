df6a35bfd9748c8c771e267aff72b709807a6af3
saruvardhan <166480247+saruvardhan@users.noreply.github.com>
2025-07-14 20:42:59 +0530

    Add files via upload

---
+++ greet function.py
@@ -0,0 +1,20 @@
+#def greet():
+ #   print('how are you')
+  #  print('whats your age')
+   # print('how is your health')
+
+#greet()    
+    
+#name=(input('\nwhats your name '))
+
+#def greet_with_name(name):
+ #   print(f'\nhow are you {name}')
+  #  print(f'whats your age {name}')
+   # print(f'how is your health mr/ms {name}')
+#greet_with_name(name)    
+
+
+def greet_with(name,location):
+    print(f'how is your health {name}')
+    print(f'did you go for teartment in {location}')
+greet_with('saru','reela')    
